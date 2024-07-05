55718569ca077dead635e7af50cebad9c9b9965a
jmfox1123 <jurre@vandegruiter.eu>
2024-07-05 14:16:11 +0200

    functions beschrijven

--- app/Livewire/Toontodo.php
+++ app/Livewire/Toontodo.php
@@ -10,14 +10,17 @@ class Toontodo extends Component
 {
     public function render()
     {
+        // maakt todo toegankelijk in toontodo blade
         return view('livewire.toontodo',[
             'todo' => todo::all(),
         ]); 
     }
     public function delete($id){
+        // verwijder todo
         todo::find($id)->delete();
     }
     public function update($id){
+        //voeg 1 toe in database zodat taak voltooid is
         todo::where('id', $id)->update([
             'Gedaan' => '1',
         ]);
--- resources/views/livewire/nieuwtodo.blade.php
+++ resources/views/livewire/nieuwtodo.blade.php
@@ -5,6 +5,5 @@
         <input class=" mt-4 mb-4 text-black rounded-md bg-gray-200 p-4 w-30 text-center" type="text" wire:model="Todo" placeholder="Taak naam"/>
         <button class="bg-green-600 p-2 rounded-md hover:bg-green-700 transition-all text-white" type="submit">opslaan</button>
         @error('Todo') <span class="error bg-red-500 p-3 mt-5 rounded-md  text-center">De taak is een verplicht veld</span> @enderror
-
     </form>  
 </div>
